0372426150a7864672229d768e92149c9025aad2
Alexandr <Akeley1994@gmail.com>
2020-03-01 03:39:55 +0200

    add applyMiddleWare

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import Timer from './components/timer';
 import reducer from './reducers/index';
 
+const MyApplyMiddleWare = (store) => (next) => (action) => {
+  // не работает с react-devtools
+  console.log('coll action: ', action);
+  const result = next(action);
+  console.log('new state: ', store.getState());
+  return result;
+};
+
 export const store = createStore(
   reducer,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__
-  // eslint-disable-next-line no-underscore-dangle
-  && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(MyApplyMiddleWare),
+  // // eslint-disable-next-line no-underscore-dangle
+  // window.__REDUX_DEVTOOLS_EXTENSION__
+  // // eslint-disable-next-line no-underscore-dangle
+  // && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 function App() {
