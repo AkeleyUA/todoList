3cca19133ddc3449f043d1fc66e9c8914ed56f4c
Alexandr <Akeley1994@gmail.com>
2020-03-25 17:23:40 +0200

    added channel

--- src/sagas/putTasks.js
+++ src/sagas/putTasks.js
@@ -1,4 +1,9 @@
-import { takeEvery, select, call, take } from 'redux-saga/effects';
+import {
+  takeEvery,
+  select,
+  call,
+  take,
+} from 'redux-saga/effects';
 import { eventChannel, END } from 'redux-saga';
 import {
   PUT_TO_LOCAL_STORAGE,
@@ -9,7 +14,7 @@ function* putTasksWorker() {
   yield select((state) => localStorage.setItem('tasks', JSON.stringify(state.tasksManager.tasks)));
 }
 
-function* channel() {
+function* catchUnloadChannel() {
   try {
     const listenerUnload = yield call(() => (
       eventChannel((emitter) => {
@@ -28,14 +33,14 @@ function* channel() {
       yield take(listenerUnload);
       yield call(putTasksWorker);
     }
-  } catch (event) {
-    console.log('error with back handler', event);
+  } catch {
+    console.log('error with listener');
   }
 }
 
 function* putTasksWatcher() {
   yield takeEvery(PUT_TO_LOCAL_STORAGE, putTasksWorker);
-  yield takeEvery(LOAD_PAGE, channel);
+  yield takeEvery(LOAD_PAGE, catchUnloadChannel);
 }
 
 export default putTasksWatcher;
